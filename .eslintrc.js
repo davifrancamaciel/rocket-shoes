4b1423185328cd529a66a29434662680ab32f3f8
Davi França Maciel <davifrancamaciel@hotmail.com>
2020-04-25 09:40:07 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'prettier',
+        'prettier/react',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-file-name-extension': [
+            'warn',
+            {
+                extensions: ['.jsx', '.js'],
+            },
+        ],
+        'import/prefer-default-export': 'off',
+    },
+}
